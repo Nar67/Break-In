4473a482746d74a54a2f98862745cf99cb197ff9
Narcis Terrado <narcis.tg@gmail.com>
2020-10-13 18:36:55 +0200

    copied some files

--- BreakIn/BreakIn/Game.cpp
+++ BreakIn/BreakIn/Game.cpp
@@ -13,7 +13,7 @@ void Game::init()
 bool Game::update(int deltaTime)
 {
 	scene.update(deltaTime);
-
+	
 	return bPlay;
 }
 
@@ -25,7 +25,7 @@ void Game::render()
 
 void Game::keyPressed(int key)
 {
-	if (key == 27) // Escape code
+	if(key == 27) // Escape code
 		bPlay = false;
 	keys[key] = true;
 }
--- BreakIn/BreakIn/Scene.cpp
+++ BreakIn/BreakIn/Scene.cpp
@@ -11,7 +11,7 @@ Scene::Scene()
 
 Scene::~Scene()
 {
-	if (map != NULL)
+	if(map != NULL)
 		delete map;
 }
 
@@ -46,13 +46,13 @@ void Scene::initShaders()
 	Shader vShader, fShader;
 
 	vShader.initFromFile(VERTEX_SHADER, "shaders/texture.vert");
-	if (!vShader.isCompiled())
+	if(!vShader.isCompiled())
 	{
 		cout << "Vertex Shader Error" << endl;
 		cout << "" << vShader.log() << endl << endl;
 	}
 	fShader.initFromFile(FRAGMENT_SHADER, "shaders/texture.frag");
-	if (!fShader.isCompiled())
+	if(!fShader.isCompiled())
 	{
 		cout << "Fragment Shader Error" << endl;
 		cout << "" << fShader.log() << endl << endl;
@@ -61,7 +61,7 @@ void Scene::initShaders()
 	texProgram.addShader(vShader);
 	texProgram.addShader(fShader);
 	texProgram.link();
-	if (!texProgram.isLinked())
+	if(!texProgram.isLinked())
 	{
 		cout << "Shader Linking Error" << endl;
 		cout << "" << texProgram.log() << endl << endl;
