00a97b15cc868ec3c0e400b5fb885f3a143680f7
YaizaCano <yaiza.cano@est.fib.upc.edu>
2020-10-14 12:32:23 +0200

    pending edit

--- BreakIn/BreakIn/Shader.h
+++ BreakIn/BreakIn/Shader.h
@@ -25,16 +25,16 @@ public:
 	Shader();
 
 	// These methods should be called with an active OpenGL context
-	void initFromSource(const ShaderType type, const string &source);
-	bool initFromFile(const ShaderType type, const string &filename);
+	void initFromSource(const ShaderType type, const string& source);
+	bool initFromFile(const ShaderType type, const string& filename);
 	void free();
 
 	GLuint getId() const;
 	bool isCompiled() const;
-	const string &log() const;
+	const string& log() const;
 
 private:
-	bool loadShaderSource(const string &filename, string &shaderSource);
+	bool loadShaderSource(const string& filename, string& shaderSource);
 
 private:
 	GLuint shaderId;
