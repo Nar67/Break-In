08d4273e75c41abcb3b3d9fb14cc33ae6a756796
Narcis Terrado <narcis.tg@gmail.com>
2020-10-24 18:21:20 +0200

    wip

--- BreakIn/BreakIn/Scene.h
+++ BreakIn/BreakIn/Scene.h
@@ -9,7 +9,7 @@
 #include "TexturedQuad.h"
 #include "Text.h"
 #include "Menu.h"
-#include <string>
+#include "Ball.h"
 
 #define CAMERA_WIDTH 640
 #define CAMERA_HEIGHT 480
@@ -42,6 +42,9 @@ private:
 private:
 	TileMap* map;
 	Player* player;
+	Ball* ball;
+	//TileMap* key;
+	//vector<TileMap*> layers;
 	ShaderProgram texProgram;
 	float currentTime;
 	glm::mat4 projection;
