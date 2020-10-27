f5c7237dbb85915f1c544e1be3d9a0c1086c9763
Narcis Terrado <narcis.tg@gmail.com>
2020-10-27 17:11:25 +0100

    redone collision detection

--- BreakIn/BreakIn/Ball.cpp
+++ BreakIn/BreakIn/Ball.cpp
@@ -56,48 +56,31 @@ void Ball::setPosition(const glm::vec2 &pos)
 void Ball::moveBall(int deltaTime)
 {
     int nextPos_x = posBall.x + speed.x * deltaTime;
-    SpriteType type = getBallTile(glm::vec2(nextPos_x, posBall.y))->getType();
-    SpriteType typeWithOffset = getBallTile(glm::vec2(nextPos_x + BALL_SIZE_X, posBall.y))->getType();
-    if(nextPos_x <= float(0))
-    {
-        speed.x *= -1;
-        nextPos_x = 0; 
-    }
-    else if(nextPos_x >= SCREEN_SIZE_X)
-    {
-        speed.x *= -1;
-        nextPos_x = SCREEN_SIZE_X; 
-    }
-    if(type == SpriteType::WALL or typeWithOffset == SpriteType::WALL)
-    {
-        speed.x *= -1;
-        nextPos_x = posBall.x;
-    }
-    
     int nextPos_y = posBall.y + speed.y * deltaTime;
-    type = getBallTile(glm::vec2(posBall.x, nextPos_y))->getType();
-    typeWithOffset = getBallTile(glm::vec2(posBall.x, nextPos_y + BALL_SIZE_Y))->getType();
-    if(nextPos_y <= float(0))
+    if(not outOfScreen(nextPos_x, nextPos_y)) //Ball is not out of screen
     {
-        speed.y *= -1;
-        nextPos_y = 0; 
-    }
-    else if(nextPos_y >= SCREEN_SIZE_Y)
-    {
-        speed.y *= -1;
-        cout << "touching SCREEN" << endl;
-        nextPos_y = SCREEN_SIZE_Y; 
-    }
-    if(type == SpriteType::WALL or typeWithOffset == SpriteType::WALL)
-    {
-        speed.y *= -1;
-        cout << "touching wall Y" << endl;
-        nextPos_y = posBall.y;
+        Tile *tile = getBallTile(glm::vec2(nextPos_x, nextPos_y)); //get the tile the ball is on
+        Tile *tileWithOffset = getBallTile(glm::vec2(nextPos_x + BALL_SIZE_X, nextPos_y + BALL_SIZE_Y));
+        if(colliding(tile, tileWithOffset)) //chech if the ball is colliding with some bounceable object
+        {
+            Tile *tileCollided = getTileColliding(tile, tileWithOffset); //get the tile that is colliding with something
+            if(collidedFromTop(nextPos_x, nextPos_y, tileCollided) or collidedFromBottom(nextPos_x, nextPos_y, tileCollided))
+            {
+                speed.y *= -1;
+                nextPos_y = posBall.y;
+                nextPos_x = posBall.x;
+            }
+            if(collidedFromRight(nextPos_x, nextPos_y, tileCollided) or collidedFromLeft(nextPos_x, nextPos_y, tileCollided))
+            {
+                speed.x *= -1;
+                nextPos_x = posBall.x;
+                nextPos_y = posBall.y;
+            }
+        }
+        posBall.x = nextPos_x;
+        posBall.y = nextPos_y;
     }
 
-    posBall.x = nextPos_x;
-    posBall.y = nextPos_y;
-    
 }
 
 Tile *Ball::getBallTile(glm::vec2 pos)
@@ -105,6 +88,77 @@ Tile *Ball::getBallTile(glm::vec2 pos)
     int tilePos_x = pos.x / map->getBlockSize();
     int tilePos_y = pos.y / (map->getBlockSize()/2);
     return map->getTiles()[tilePos_y * map->getMapSize().x + tilePos_x];
+}
+
+Tile *Ball::getTileColliding(Tile* tile1, Tile* tile2)
+{
+    if(tile1->getType() == SpriteType::WALL or tile1->getType() == SpriteType::BLOCK) //or tile1->getType() == SpriteType::PLATFORM)
+    {
+        return tile1;
+    }
+    else
+    {
+        return tile2;
+    }
+}
+
+bool Ball::colliding(Tile* tile1, Tile* tile2)
+{
+    return tile1->getType() == SpriteType::WALL or tile1->getType() == SpriteType::BLOCK or 
+        tile2->getType() == SpriteType::WALL or tile2->getType() == SpriteType::BLOCK; //or tile2->getType() == SpriteType::PLATFORM //or tile1->getType() == SpriteType::PLATFORM)
+}
+
+bool Ball::collidedFromLeft(int next_x, int next_y, Tile* tile)
+{
+    return posBall.x + BALL_SIZE_X < tile->getPosition().x &&
+            next_x + BALL_SIZE_X >= tile->getPosition().x;
+}
+
+bool Ball::collidedFromRight(int next_x, int next_y, Tile* tile)
+{
+    return posBall.x >= tile->getPosition().x + tile->getBlockSize() &&
+            next_x < tile->getPosition().x + tile->getBlockSize();
+}
+
+bool Ball::collidedFromBottom(int next_x, int next_y, Tile* tile)
+{
+    return posBall.y >= tile->getPosition().y + tile->getBlockSize() &&
+            next_y < tile->getPosition().y + tile->getBlockSize();
+}
+
+bool Ball::collidedFromTop(int next_x, int next_y, Tile* tile)
+{
+    return posBall.y + BALL_SIZE_Y < tile->getPosition().y &&
+            next_y + BALL_SIZE_Y >= tile->getPosition().y;
+}
+
+bool Ball::outOfScreen(int nextPos_x, int nextPos_y)
+{
+    if(nextPos_x <= float(0))
+    {
+        speed.x *= -1;
+        posBall.x = 1;
+        return true;
+    }
+    else if(nextPos_x >= SCREEN_SIZE_X)
+    {
+        speed.x *= -1;
+        posBall.x = SCREEN_SIZE_X-1; 
+        return true;
+    }
+    if(nextPos_y <= float(0))
+    {
+        speed.y *= -1;
+        posBall.y = 1;
+        return true;
+    }
+    else if(posBall.y >= SCREEN_SIZE_Y)
+    {
+        speed.y *= -1;
+        posBall.y = SCREEN_SIZE_Y-1;
+        return true;
+    }
+    return false;
 }
 
 void Ball::printBallTile()
