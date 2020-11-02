2cf8e64978f02462ed17c8cf64a162424555ed9d
Narcis Terrado <narcis.tg@gmail.com>
2020-11-02 09:20:47 +0100

    player bouncing

--- BreakIn/BreakIn/Player.cpp
+++ BreakIn/BreakIn/Player.cpp
@@ -5,6 +5,8 @@
 #include "Player.h"
 #include "Game.h"
 
+#define PLAYER_SIZE_X 34
+#define PLAYER_SIZE_Y 46
 Player::Player()
 {
 }
@@ -17,7 +19,7 @@ Player::~Player()
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	spritesheet.loadFromFile("images/player.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(34, 46), glm::vec2(1, 1), &spritesheet, &shaderProgram);
+	sprite = Sprite::createSprite(glm::ivec2(PLAYER_SIZE_X, PLAYER_SIZE_Y), glm::vec2(1, 1), &spritesheet, &shaderProgram);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
 }
@@ -71,6 +73,16 @@ void Player::setPosition(const glm::vec2 &pos)
 {
 	posPlayer = pos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+}
+
+glm::ivec2 Player::getPosition()
+{
+	return posPlayer + tileMapDispl;
+}
+
+int Player::getPlayerXSize()
+{
+	return PLAYER_SIZE_X;
 }
 
 
